09522c198130b0de06843d138c9ff576421c058e
xiangpengBu <xiangpeng.bu@gmail.com>
2020-06-02 19:21:28 +0800

    test

---
+++ GR.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'GR.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtWidgets
+
+from PyQt5.QtWidgets import QApplication, QGraphicsScene, QMainWindow, QGraphicsPixmapItem
+from PyQt5.QtGui import QImage, QPixmap
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(180, 480, 91, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(350, 480, 91, 31))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(520, 480, 91, 31))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(240, 370, 54, 12))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(490, 370, 54, 12))
+        self.label_2.setObjectName("label_2")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(180, 40, 431, 301))
+        self.graphicsView.setObjectName("graphicsView")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(190, 400, 161, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(440, 400, 161, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton_3.clicked.connect(MainWindow.close)
+        self.pushButton.clicked.connect(MainWindow.capture)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Gesture Recognition"))
+        self.pushButton.setText(_translate("MainWindow", "截取"))
+        self.pushButton_2.setText(_translate("MainWindow", "识别"))
+        self.pushButton_3.setText(_translate("MainWindow", "关闭"))
+        self.label.setText(_translate("MainWindow", "偏转角度"))
+        self.label_2.setText(_translate("MainWindow", "中心坐标"))
+
+
+def main():
+    import sys
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    gui = Ui_MainWindow()
+    gui.setupUi(MainWindow)
+    MainWindow.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+
+    main()
